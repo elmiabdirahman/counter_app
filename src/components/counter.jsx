6db6dfce4afd78ac40b3a5a50af8f64e9b567d27
Elmi Elmi <elmielmi522@gmail.com>
2020-12-22 03:12:40 -0600

    add classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,13 +4,20 @@ class Counter extends Component {
     state = {
         count: 0
     };
+
     render() { 
         return (
-            <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondry btn-sm">Increment</button>
+            </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
