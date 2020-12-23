f727d5539a16aaecc3bce2f8cad16da575ae184e
Elmi Elmi <elmielmi522@gmail.com>
2020-12-23 00:37:06 -0600

    onclick events in counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        count: 0
     };
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags</p>; 
-            return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    handleIncrement() {
+        console.log('Increment Clicket', this);
     }
 
     render() { 
-        return <div>{this.state.tags.length === 0 && "Please create a new tag"}{this.renderTags()}</div>
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondry btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
