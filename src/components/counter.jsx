746c8e5ac271028ea065792dc859e2bfc6499401
Elmi Elmi <elmielmi522@gmail.com>
2020-12-23 01:34:17 -0600

    events handle

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,24 @@ class Counter extends Component {
         count: 0
     };
 
-    handleIncrement = () => {
-        console.log('Increment Clicket', this);
+    handleIncrement = (event) => {
+        console.log(event);
         this.setState({
             count: this.state.count + 1
-        })
+        });
+    }
+
+    doHandleIncrement = () => {
+        this.handleIncrement({
+            id: 1
+        });
     }
 
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondry btn-sm">Increment</button>
+                <button onClick={this.doHandleIncrement} className="btn btn-secondry btn-sm">Increment</button>
             </div>
         );
     }
